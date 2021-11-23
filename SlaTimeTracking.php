72d7f3925730b75370448b37a86540be8250d883
Sciecha <scieszkamk@gmail.com>
2021-11-23 16:41:22 +0100

    create table for sla time tracking

--- SlaTimeTracking.php
+++ SlaTimeTracking.php
@@ -17,11 +17,36 @@ class SlaTimeTracking extends MantisPlugin
     function hooks() {
         return array(
             'EVENT_MENU_MAIN' => 'menu',
-
             'EVENT_UPDATE_BUG' => 'updateBug'
         );
     }
 
+    function updateBug() {
 
+    }
+
+    function schema() {
+        $t_schema = array();
+
+        $t_table = plugin_table( 'time_tracking' );
+        $t_ddl = " id  I   NOTNULL UNSIGNED PRIMARY AUTOINCREMENT,
+                 bug_id I DEFAULT NULL UNSIGNED,
+                 start_date T DEFAULT NULL,
+                 end_date T DEFAULT NULL,
+                 sla_time I DEFAULT 0,
+                 suspended_start_date T DEFAULT NULL,
+                 suspended_end_date T DEFAULT NULL,
+                 suspended_time I DEFAULT 0";
+
+        $t_schema[] = array( 'CreateTableSQL',
+            array($t_table , $t_ddl) );
+
+        $t_schema[] = array( 'CreateIndexSQL',
+            array( 'idx_bug_id_sla_time_tracking',
+                $t_table,
+                'bug_id', array( 'UNIQUE' ) ) );
+
+        return $t_schema;
+    }
 
 }
